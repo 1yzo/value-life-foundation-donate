0659e52999a77c8f2cd4072dc5e78d869cca653f
Osman Ali <okali@uci.edu>
2018-06-19 15:46:58 -0700

    Changed carousel switch index from number to dots

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -8,6 +8,18 @@ class Carousel extends React.Component {
         shouldHide: false
     };
 
+    createIndexDots = () => {
+        const dots = [];
+        for (let i = 0; i < this.props.items.length; i++) {
+            dots.push(<div 
+                            className={"carousel__switch__dot" + (i === this.state.index ? " carousel__switch__dot--active" : "")} 
+                            style={{ background: this.props.switchColor }} 
+                            key={i} 
+                        />)
+        }
+        return dots;
+    }
+
     handleClick = () => {
         setTimeout(this.changeItem, 500);
         this.setState(() => ({ shouldHide: true }));
@@ -32,15 +44,18 @@ class Carousel extends React.Component {
     render() {
         return (
             <div className="carousel">
-                <div className={"carousel-item" + (this.state.shouldHide ? " carousel-item--hidden" : "")} 
+                <div
+                    className={"carousel-item" + (this.state.shouldHide ? " carousel-item--hidden" : "")} 
                     onClick={this.handleClick} 
                 >
                     {this.props.items[this.state.index]}
                 </div>
                 <div className="carousel__switch" style={{ color: this.props.switchColor }}>
-                    <i className="material-icons">keyboard_arrow_left</i>
-                    {this.state.index + 1}
-                    <i className="material-icons">keyboard_arrow_right</i>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                        <i className="material-icons">keyboard_arrow_left</i>
+                        {this.createIndexDots()}
+                        <i className="material-icons">keyboard_arrow_right</i>
+                    </div>
                 </div>
             </div>
         );
