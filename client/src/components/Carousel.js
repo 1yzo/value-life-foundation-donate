b86e6026e8884b4b0657a4e55a05b5fea807d09c
Osman Ali <okali@uci.edu>
2018-06-20 06:30:16 -0700

    Added directional switching to the carousel with button transitions

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -20,23 +20,38 @@ class Carousel extends React.Component {
         return dots;
     }
 
-    handleClick = () => {
-        setTimeout(this.changeItem, 500);
+    handleClick = (e) => {
+        const direction = e.target.attributes.name.value;
+        setTimeout(this.changeItem.bind(null, direction), 500);
         this.setState(() => ({ shouldHide: true }));
     };
 
-    changeItem = () => {
+    changeItem = (direction) => {
         this.setState((prevState) => {
-            if (prevState.index + 1 < this.props.items.length) {
-                return { 
-                    index: prevState.index + 1,
-                    shouldHide: false
-                };
-            } else {
-                return {
-                    index: 0,
-                    shouldHide: false
-                };
+            if (direction === 'right') {
+                if (prevState.index + 1 < this.props.items.length) {
+                    return { 
+                        index: prevState.index + 1,
+                        shouldHide: false
+                    };
+                } else {
+                    return {
+                        index: 0,
+                        shouldHide: false
+                    };
+                }
+            } else if (direction === "left") {
+                if (prevState.index - 1 >= 0) {
+                    return {
+                        index: prevState.index - 1,
+                        shouldHide: false
+                    };
+                } else {
+                    return {
+                        index: this.props.items.length - 1,
+                        shouldHide: false
+                    };
+                }
             }
         });
     };
@@ -45,17 +60,16 @@ class Carousel extends React.Component {
         return (
             <div className="carousel">
                 <div
-                    className={"carousel-item" + (this.state.shouldHide ? " carousel-item--hidden" : "")} 
-                    onClick={this.handleClick} 
+                    className={"carousel-item" + (this.state.shouldHide ? " carousel-item--hidden" : "")}  
                 >
                     {this.props.items[this.state.index]}
                 </div>
                 <div className="carousel__switch" style={{ color: this.props.switchColor }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                        <i className="material-icons">keyboard_arrow_left</i>
+                        <i className="material-icons switch__arrow" name="left" onClick={this.handleClick}>keyboard_arrow_left</i>
                         {this.createIndexDots()}
-                        <i className="material-icons">keyboard_arrow_right</i>
-                    </div>
+                        <i className="material-icons switch__arrow" name="right" onClick={this.handleClick} >keyboard_arrow_right</i>
+                    </div> 
                 </div>
             </div>
         );
