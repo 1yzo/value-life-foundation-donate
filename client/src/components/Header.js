0d6dc2adf332b37d34ec82c98e555e76bfb20589
Osman Ali <okali@uci.edu>
2018-06-25 02:59:37 -0700

    Fixed scroll offset

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,19 +11,19 @@ class Header extends React.Component {
         return <div className="header">
             <h1 className="header-link" onClick={this.scrollToTop}>VLF</h1>
             <div className="links-section">
-                <Link className="link" activeClass="link--active" to="about-us-page" spy smooth duration={500}>
+                <Link className="link" activeClass="link--active" to="about-us-page" spy smooth duration={500} offset={-70}>
                     <h4>ABOUT US</h4>
                 </Link>
-                <Link className="link" activeClass="link--active" to="programs-page" spy smooth duration={500}>
+                <Link className="link" activeClass="link--active" to="programs-page" spy smooth duration={500} offset={-70}>
                     <h4>PROGRAMS</h4>
                 </Link>
-                <Link className="link" activeClass="link--active" to="current-projects-page" spy smooth duration={500}>
+                <Link className="link" activeClass="link--active" to="current-projects-page" spy smooth duration={500} offset={-70}>
                     <h4>CURRENT PROJECTS</h4>
                 </Link>
-                <Link className="link" activeClass="link--active" to="links-page" spy smooth duration={500}>
+                <Link className="link" activeClass="link--active" to="links-page" spy smooth duration={500} offset={-70}>
                     <h4>LINKS</h4>
                 </Link>
-                <Link className="link" to="donate-page" spy smooth duration={500}>
+                <Link className="link" to="donate-page" spy smooth duration={500} offset={-70}>
                     <h4 className="link--donate">DONATE</h4>
                 </Link>
             </div>
