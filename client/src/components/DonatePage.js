096c44a21786a8ddd02a439b2fb6e6f4e2676cda
Osman Ali <okali@uci.edu>
2018-06-25 02:18:24 -0700

    Fixed formatting

--- client/src/components/DonatePage.js
+++ client/src/components/DonatePage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Header from './Header';
-
+import '../styles/page.css';
 
 const DonatePage = (props) => (
-    <div>
+    <div className="page">
         Donate
     </div>
 );
