cabcbd6dba53b38fbaf2a4aa1c580d19ad4c15f9
Osman Ali <okali@uci.edu>
2018-06-25 00:41:04 -0700

    Removed header component and scrolling working

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,23 +1,68 @@
 import React from 'react';
 import '../styles/page.css';
 import '../styles/container.css';
+import '../styles/header.css';
 import WelcomePage from './WelcomePage'
 import ProgramsPage from './ProgramsPage';
 import CurrentProjectsPage from './CurrentProjectsPage';
 import LinksPage from './LinksPage';
+import { Link, Element, Events, animateScroll, scrollSpy, scroller } from 'react-scroll';
 
 class Main extends React.Component {
+    componentDidMount() {
+        Events.scrollEvent.register('begin', function (to, element) {
+            console.log('begin', arguments);
+        });
+
+        Events.scrollEvent.register('end', function (to, element) {
+            console.log('end', arguments);
+        });
+
+        scrollSpy.update();
+    }
+    componentWillUnmount() {
+        Events.scrollEvent.remove('begin');
+        Events.scrollEvent.remove('end');
+    }
+
+    scrollToTop = () => {
+        animateScroll.scrollToTop();
+    }
+
     render() {
         return (
-            <div className="container">
-                <WelcomePage />
-                <ProgramsPage storeRef={this.props.storeRef}/>
-                <CurrentProjectsPage />
-                <LinksPage />
+            <div>
+                <div className="header">
+                    <h1 className="header-link" onClick={this.scrollToTop}>VLF</h1>
+                    <div className="links-section">
+                        <Link className="link" to="programs-page" spy smooth duration={500}>
+                            Programs
+                        </Link>
+                        <Link className="link" to="links-page" spy smooth duration={500}>
+                            Links
+                        </Link>
+                        <Link className="link" to="current-projects-page" spy smooth duration={500}>
+                            Current Projects
+                        </Link>
+                    </div>
+                </div>
+                <div className="container">
+                    <WelcomePage />
+                    <Element name="programs-page">
+                        <ProgramsPage />
+                    </Element>
+                    <Element name="current-projects-page">
+                        <CurrentProjectsPage />
+                    </Element>
+                    <Element name="links-page">
+                        <LinksPage />
+                    </Element>
+                </div>
             </div>
         );
-      
+
     }
 }
 
 export default Main;
+
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from '../components/Main';
 import DonatePage from '../components/DonatePage';
-import Header from '../components/Header';
 
 class AppRouter extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div style={{ display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-                    <Header />
+                <div style={{ display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
                     <Switch>
-                        <Route path="/" render={(props) => <Main />} exact={true} />
+                        <Route path="/" component={Main} exact />
                         <Route path="/donate" component={DonatePage} />
                     </Switch>
                 </div>
