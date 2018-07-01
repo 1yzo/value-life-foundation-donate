fa7d286438377e7771bc7e3f803050ce44fa65fa
Osman Ali <okali@uci.edu>
2018-06-30 23:38:29 -0700

    Implementd stripe online paymentws

--- client/src/components/PaymentCheckout.js
+++ client/src/components/PaymentCheckout.js
@@ -1,15 +1,31 @@
 import React from 'react';
-
+import StripeCheckout from 'react-stripe-checkout';
+import '../styles/donate.css';
+import secrets from  '../secrets';
 
 class PaymentCheckout extends React.Component {
-    componentDidMount() {
-        
+    onToken = (token) => {
+        console.log(token);
+        fetch('/api/charge', {
+            method: 'POST',
+            body: JSON.stringify({
+                token: 'tok_visa',
+                amount: 2000
+            }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
     }
     
     render() {
         return (
             <div className="page">
-                Checkout
+                <StripeCheckout
+                    token={this.onToken}
+                    stripeKey={secrets.clientKey}
+                    description="Testing"
+                />
             </div>
         );
     }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const braintree = require('braintree');
+const secrets = require('../../secrets');
+const stripe = require('stripe')(secrets.serverKey);
 
-
+router.post('/charge', (req, res, next) => {
+    const { amount, token } = req.body;
+    const charge = stripe.charges.create({
+        amount,
+        currency: 'usd',
+        source: token,
+    }, (err, charge) => console.log(err));
+    res.json(charge);
+});
 
 module.exports = router;
