afdab6b30807bfa5db674a641fade3805a622ffb
Osman Ali <okali@uci.edu>
2018-07-03 14:56:30 -0700

    Added floating menu action button on mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, animateScroll } from 'react-scroll';
 import '../styles/header.css';
+import FloatingMenu from './FloatingMenu';
 
 class Header extends React.Component {
     scrollToTop = () => {
@@ -9,27 +10,30 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="header">
-                <div className="placeholder"/>
-                <h1 className="header-link" onClick={this.scrollToTop}>VLF</h1>
-                <div className="links-section">
-                    <Link className="link desktop-only" activeClass="link--active" to="about-us-page" spy smooth duration={500} offset={-70}>
-                        <h4>ABOUT US</h4>
-                    </Link>
-                    <Link className="link desktop-only" activeClass="link--active" to="programs-page" spy smooth duration={500} offset={-70}>
-                        <h4>PROGRAMS</h4>
-                    </Link>
-                    <Link className="link desktop-only" activeClass="link--active" to="current-projects-page" spy smooth duration={500} offset={-70}>
-                        <h4>CURRENT PROJECTS</h4>
-                    </Link>
-                    <Link className="link desktop-only" activeClass="link--active" to="links-page" spy smooth duration={500} offset={-70}>
-                        <h4>LINKS</h4>
-                    </Link>
-                    <Link className="link"  style={{ paddingRight: 0} } to="donate-page" spy smooth duration={500} offset={-70}>
-                        <h4 className="link--donate">DONATE</h4>
-                    </Link>
+            <div>
+                <div className="header">
+                    <div className="placeholder"/>
+                    <h1 className="header-link" onClick={this.scrollToTop}>VLF</h1>
+                    <div className="links-section">
+                        <Link className="link desktop-only" activeClass="link--active" to="about-us-page" spy smooth duration={500} offset={-70}>
+                            <h4>ABOUT US</h4>
+                        </Link>
+                        <Link className="link desktop-only" activeClass="link--active" to="programs-page" spy smooth duration={500} offset={-70}>
+                            <h4>PROGRAMS</h4>
+                        </Link>
+                        <Link className="link desktop-only" activeClass="link--active" to="current-projects-page" spy smooth duration={500} offset={-70}>
+                            <h4>CURRENT PROJECTS</h4>
+                        </Link>
+                        <Link className="link desktop-only" activeClass="link--active" to="links-page" spy smooth duration={500} offset={-70}>
+                            <h4>LINKS</h4>
+                        </Link>
+                        <Link className="link"  style={{ paddingRight: 0} } to="donate-page" spy smooth duration={500} offset={-70}>
+                            <h4 className="link--donate">DONATE</h4>
+                        </Link>
+                    </div>
+                    <div className="placeholder"/>
                 </div>
-                <div className="placeholder"/>
+                <FloatingMenu />
             </div>
         );
     }
