09d72ad0b0c4cb994d900411eee55484c6687c97
Osman Ali <okali@uci.edu>
2018-07-03 16:03:43 -0700

    Floating action menu is now functional

--- client/src/components/FloatingMenu.js
+++ client/src/components/FloatingMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/FloatingMenu.css';
+import { Link } from 'react-scroll';
 
 class FloatingMenu extends React.Component {
     state = {
@@ -15,9 +16,26 @@ class FloatingMenu extends React.Component {
             <div 
                 className={"floating-menu" + (this.state.shouldExpand ? " floating-menu--expanded" : "")}
                 onClick={this.handleExpand}
+                onTransitionEnd={this.handleTransitionEnd}
             >
-                {this.state.shouldExpand && <h1>Menu</h1>}
-                <i className="material-icons drawer">menu</i>
+                {!this.state.shouldExpand && <i className="material-icons drawer">menu</i>}
+                {
+                    this.state.shouldExpand &&
+                    <div className="links-section--mobile">
+                        <Link onClick={this.handleExpand} className="link link--mobile" to="about-us-page" spy smooth duration={500} offset={-70}>
+                            <h4>ABOUT US</h4>
+                        </Link>
+                        <Link onClick={this.handleExpand} className="link link--mobile" to="programs-page" spy smooth duration={500} offset={-70}>
+                            <h4>PROGRAMS</h4>
+                        </Link>
+                        <Link onClick={this.handleExpand} className="link link--mobile" to="current-projects-page" spy smooth duration={500} offset={-70}>
+                            <h4>CURRENT PROJECTS</h4>
+                        </Link>
+                        <Link onClick={this.handleExpand} className="link link--mobile" to="links-page" spy smooth duration={500} offset={-70}>
+                            <h4>LINKS</h4>
+                        </Link>
+                    </div>
+                }
             </div>
         );
     }
