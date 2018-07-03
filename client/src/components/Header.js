1c7b091145b3c00243ff438b98785b9ebb670e20
Osman Ali <okali@uci.edu>
2018-07-03 01:15:39 -0700

    Added donate button to mobile header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,16 +13,16 @@ class Header extends React.Component {
                 <div className="placeholder"/>
                 <h1 className="header-link" onClick={this.scrollToTop}>VLF</h1>
                 <div className="links-section">
-                    <Link className="link" activeClass="link--active" to="about-us-page" spy smooth duration={500} offset={-70}>
+                    <Link className="link desktop-only" activeClass="link--active" to="about-us-page" spy smooth duration={500} offset={-70}>
                         <h4>ABOUT US</h4>
                     </Link>
-                    <Link className="link" activeClass="link--active" to="programs-page" spy smooth duration={500} offset={-70}>
+                    <Link className="link desktop-only" activeClass="link--active" to="programs-page" spy smooth duration={500} offset={-70}>
                         <h4>PROGRAMS</h4>
                     </Link>
-                    <Link className="link" activeClass="link--active" to="current-projects-page" spy smooth duration={500} offset={-70}>
+                    <Link className="link desktop-only" activeClass="link--active" to="current-projects-page" spy smooth duration={500} offset={-70}>
                         <h4>CURRENT PROJECTS</h4>
                     </Link>
-                    <Link className="link" activeClass="link--active" to="links-page" spy smooth duration={500} offset={-70}>
+                    <Link className="link desktop-only" activeClass="link--active" to="links-page" spy smooth duration={500} offset={-70}>
                         <h4>LINKS</h4>
                     </Link>
                     <Link className="link"  style={{ paddingRight: 0} } to="donate-page" spy smooth duration={500} offset={-70}>
