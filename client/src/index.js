1ac931704829fab504c78baf5ab48d4861338b98
Osman Ali <okali@uci.edu>
2018-09-08 15:42:00 -0700

    Remove redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import './styles/base.css';
-import configureStore from './store/configureStore';
 import Main from './components/Main';
 
-const store = configureStore();
-
 const jsx = (
-    <Provider  store={store}>
-        <Main />
-    </Provider>
+    <Main />
 );
 
 ReactDOM.render(jsx, document.getElementById('root'));
