8dde372d3e1c451f4ff58f17ffbc45dd969b20bd
Noitidart <noitidart@gmail.com>
2020-10-10 10:31:20 -0700

    Make formatting changes to test deployment

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -4,18 +4,24 @@ import '../styles/page.css';
 import '../styles/backgrounds.css';
 
 const WelcomePage = ({ backgroundStyle }) => (
-    <div className="page page--first welcome" style={backgroundStyle}>
-        <div style={{ flex: 1 }} />
-        <div className="page__content" style={{ color: 'white', marginTop: '3rem' }}>
-            <h1 className="page__content__header">Welcome to <br/>  Value Life Foundation</h1>
-            <p>
-                Value Life Foundation (VLF) is established in Claremont, California, USA in January 2002
-                as a nonprofit, public benefit corporation. It is organized under the nonprofit public benefit
-                corporation law for charitable, educational, and scientific purposes.
-            </p>
-            <p style={{ fontSize: 'smaller'}}>Our US Federal Tax # is: 68-0493506</p>
-        </div>
+  <div className="page page--first welcome" style={backgroundStyle}>
+    <div style={{ flex: 1 }} />
+    <div
+      className="page__content"
+      style={{ color: 'white', marginTop: '3rem' }}
+    >
+      <h1 className="page__content__header">
+        Welcome to <br /> Value Life Foundation
+      </h1>
+      <p>
+        Value Life Foundation (VLF) is established in Claremont, California, USA
+        on January 2002 as a nonprofit, public benefit corporation. It is
+        organized under the nonprofit public benefit corporation law for
+        charitable, educational, and scientific purposes.
+      </p>
+      <p style={{ fontSize: 'smaller' }}>Our US Federal Tax # is: 68-0493506</p>
     </div>
+  </div>
 );
 
 export default WelcomePage;
